84446109ddf760020159742b9c4f79934d7a6eae
Carkzis <marc.jowett@gmail.com>
2022-04-14 11:56:48 +0100

    Add movie that plays on click of play button

--- Theia/ViewController.m
+++ Theia/ViewController.m
@@ -16,8 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
+    // Do any additional setup after loading the view.
     [self setUpPlayButton];
 }
 
@@ -28,13 +28,24 @@
     playButton.center = self.view.center;
     
     // Add a target method to the button for when it is clicked.
-    [playButton addTarget:self action:@selector(play:) forControlEvents:UIControlEventPrimaryActionTriggered];
+    [playButton addTarget:self action:@selector(playMovie:) forControlEvents:UIControlEventPrimaryActionTriggered];
     
     [self.view addSubview:playButton];
 }
 
-- (void)play:(UIButton *)playButton {
+- (void)playMovie:(UIButton *)playButton {
     NSLog(@"The play button has been pressed!");
+    NSURL *url = [[NSURL alloc]
+                  initWithString:@"https://ia800300.us.archive.org/1/items/night_of_the_living_dead/night_of_the_living_dead_512kb.mp4"];
+    [self playMedia:url];
+}
+
+- (void)playMedia:(NSURL *)url {
+    AVPlayer *player = [AVPlayer playerWithURL:url];
+    AVPlayerViewController *controller = [[AVPlayerViewController alloc] init];
+    [self presentViewController: controller animated: YES completion: nil];
+    controller.player = player;
+    [player play];
 }
 
 @end
